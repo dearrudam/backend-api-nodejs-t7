fe5830b1afc113719f2e4b26172a4344633e9fba
wacoders <w.silva@qacoders-treinamento.com>
2022-05-11 10:57:21 -0300

    configuration api register

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const port = require('./routes')
 
-module.exports = function(server) {
-    const protectApi = express.Router();
-    server.use("/api", protectApi);
+module.exports = function (server) {
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
 
-    server.use("/status", (req, res) =>
-        res.send(`BACKEND is runner.`)    
+    server.use('/status', (req, res) =>
+        res.send(`BACKEND is runner.`)
     );
 
-    server.use(express.static(require("path").join(__dirname, "../public")))
+    const register = require('../api/register/registerService');
+    register.register(protectedApi, '/register');
+
+    server.use(express.static(require('path').join(__dirname, '../public')));
 }
