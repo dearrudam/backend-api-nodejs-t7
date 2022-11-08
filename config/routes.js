6b98b46f707d4e76e9c8c4896e0a525b8de426e1
Maximillian Arruda <dearrudam@gmail.com>
2022-11-08 20:56:30 -0300

    Decouplying API resources at router.js

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,16 @@
-const express = require('express')
+const express = require("express");
+const apiRegistry = require("./api-registry.json");
 
 module.exports = function (server) {
-    const protectedApi = express.Router();
-    server.use('/api', protectedApi);
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
 
-    server.use('/status', (req, res) =>
-        res.send(`BACKEND is runner.`)
-    );
+  server.use("/status", (req, res) => res.send(`BACKEND is runner.`));
 
-    const register = require('../api/register/registerService');
-    register.register(protectedApi, '/register');
+  Object.keys(apiRegistry).forEach((path) => {
+    const service = require(apiRegistry[path]);
+    service.register(protectedApi, path);
+  });
 
-    server.use(express.static(require('path').join(__dirname, '../public')));
-}
+  server.use(express.static(require("path").join(__dirname, "../public")));
+};
